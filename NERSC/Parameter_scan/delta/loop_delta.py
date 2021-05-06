fd2e9e61dfd17e0143b992a8a3dc88ab96a8dd63
Nan Wang <38221905+nanw0321@users.noreply.github.com>
2021-05-06 08:46:00 +0800

    Update: Strehl ratio calculation using projection of beam instead of slice

--- NERSC/Parameter_scan/delta/loop_delta.py
+++ NERSC/Parameter_scan/delta/loop_delta.py
@@ -16,9 +16,9 @@ if_close = 0
 # generate scripts and submit job
 err_delta_list = np.linspace(-1000, 1000, 50)	# [nrad]
 for job_num, err_delta in enumerate(err_delta_list):
-    # os.system(
-    #     "python Gen_job_script.py --bl {} --var_t {} --err_E {} --err_f0 {} --err_f1 {} --C {} --err_delta {} --err_miscut {} --if_log {} --if_slice {} --if_close {} --job {}".format(
-    #         bl, var_t, err_E, err_f0, err_f1, C, err_delta, err_miscut, if_log, if_slice, 0, job_num+1))
+    os.system(
+        "python Gen_job_script.py --bl {} --var_t {} --err_E {} --err_f0 {} --err_f1 {} --C {} --err_delta {} --err_miscut {} --if_log {} --if_slice {} --if_close {} --job {}".format(
+            bl, var_t, err_E, err_f0, err_f1, C, err_delta, err_miscut, if_log, if_slice, 0, job_num+1))
     os.system(
         "python Gen_job_script.py --bl {} --var_t {} --err_E {} --err_f0 {} --err_f1 {} --C {} --err_delta {} --err_miscut {} --if_log {} --if_slice {} --if_close {} --job {}".format(
             bl, var_t, err_E, err_f0, err_f1, C, err_delta, err_miscut, if_log, if_slice, 1, job_num+1))
